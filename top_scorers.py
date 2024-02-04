5a33741bd3d36a5ce77a274e68b1ccf2ab330733
idobrovolska <ilonadbrvlsk@gmail.com>
2024-02-04 16:33:11 +0200

    Added top_scorers.py

---
+++ top_scorers.py
@@ -0,0 +1,5 @@
+{'Premier League': [('Erling Haaland', 25)],
+ 'Ligue 1': [('Folarin Balogun', 14), ('Wissam Ben Yedder', 14)],
+ 'Serie A': [('Victor Osimhen', 16)],
+ 'La Liga': [('Robert Lewandowski', 14)],
+ 'Bundesliga': [('Niclas Füllkrug', 13)]}
